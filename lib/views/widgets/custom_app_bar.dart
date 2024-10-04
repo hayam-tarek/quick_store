1deb622fc989153f4b253ac3787bc60c276ec796
hayam-tarek <hayamtareq28@gmail.com>
2024-10-04 04:28:39 +0300

    add menu drawer

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,30 +1,44 @@
-import 'package:quick_store/core/utils/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:quick_store/core/utils/constant.dart';
 
 AppBar customAppBar() {
   return AppBar(
     centerTitle: true,
     backgroundColor: kBackgroundColor,
-    title: const Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Quick',
-          style: TextStyle(
-            fontFamily: kPrimaryFont,
-            fontSize: 35,
-            color: kPrimaryColor,
+    title: IntrinsicWidth(
+      child: const Row(
+        children: [
+          Text(
+            'Quick',
+            style: TextStyle(
+              fontFamily: kPrimaryFont,
+              fontSize: 35,
+              color: kPrimaryColor,
+            ),
           ),
-        ),
-        Text(
-          'Store',
-          style: TextStyle(
-            fontFamily: kPrimaryFont,
-            fontSize: 35,
-            color: kSecondaryColor,
+          Text(
+            'Store',
+            style: TextStyle(
+              fontFamily: kPrimaryFont,
+              fontSize: 35,
+              color: kSecondaryColor,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
+    actions: [
+      Builder(builder: (context) {
+        return IconButton(
+          onPressed: () {
+            Scaffold.of(context).openEndDrawer();
+          },
+          icon: Image.asset(
+            kMenuIconPath,
+            scale: 17.5,
+          ),
+        );
+      }),
+    ],
   );
 }
